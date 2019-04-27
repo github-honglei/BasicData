d5bb4851285c3d47614793cd5328c52e2f569082
Mr.Wang <594784309@qq.com>
2019-04-27 23:38:25 +0800

    第一天

---
+++ BasicDataOperate/src/com/honglei/data/operate/StringOperate.java
@@ -0,0 +1,13 @@
+package com.honglei.data.operate;
+
+public class StringOperate {
+	
+	public static void main(String[] args) {
+		String operateStr = "Study Java";
+		
+		operateStr.equals("123");
+		
+		operateStr.substring(1);
+		
+	}
+}
